817d6d979fa874544d5aaf4595f9dc234ba7200e
pew-rivza <dz-10b@ya.ru>
2022-03-29 20:44:23 +0300

    configs added

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: path.resolve(__dirname, 'node_modules'),
+            }
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        }),
+    ]
+}
