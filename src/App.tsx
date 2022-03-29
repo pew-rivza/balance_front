e74cbe129051af582134db0835233be07ccec5dd
pew-rivza <dz-10b@ya.ru>
2022-03-29 21:32:46 +0300

    eslint + prettier added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React from "react";
 
-export default function App()
-{
-  return <h1>Hello, world!</h1>
+export default function App() {
+  return (
+    <h1>
+      Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello,
+      world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world!
+    </h1>
+  );
 }
