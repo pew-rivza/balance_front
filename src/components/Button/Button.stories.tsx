2761e2f732407bc0183a0cc8c1e4ca28d44e17d3
pew-rivza <dz-10b@ya.ru>
2022-05-13 22:45:13 +0300

    button story refactoring

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,23 +1,41 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { fireEvent, screen } from '@storybook/testing-library';
 
 // TODO: Доделать сторис кнопки
+// TODO: посмотреть, какие есть аддоны и добавить нужные
 
 import Button from "./Button";
 
 export default {
-  title: "Components/Button",
+  title: "Simple Components/Button",
   component: Button,
+  argTypes: {
+    type: {table: {disable: true}},
+    onClick: {control: false, action: 'clicked'},
+    classNames: {control: false},
+  },
+  parameters: {
+    actions: {
+      // argTypesRegex: '^on.*',
+      handles: ['click'],
+    },
+  },
 } as ComponentMeta<typeof Button>;
+
+// TODO: узнать по поводу actions в сторибуке и настроить их
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 
+Default.storyName = "Button";
 Default.args = {
   value: "Добавить",
   onClick: () => console.log("clicked from storybook!1"),
 };
+Default.play = async () => {
+  await fireEvent.click(screen.getByRole('button'));
+};
 
-// TODO: доделать сторис для кнопки
 // TODO: написать тесты для кнопки (визуальные и функциональные)
