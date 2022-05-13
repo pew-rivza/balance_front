d42284358c78afde7f72c6056f82aa169b9d8729
pew-rivza <dz-10b@ya.ru>
2022-05-13 20:27:37 +0300

    linter fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Button from "components/Button";
 
-export default function App() {
+const App = () => {
   return (
     <>
-      <h1>
-        Hello, world!
-      </h1>
-      <Button value={"Hello rvz 8!"}/>
+      <h1>Hello, world!</h1>
+      <Button value="Hello rvz 8!" onClick={() => console.log("rvz clicked!1")} disabled />
+      <h2>Пока-пока, мир!</h2>
     </>
   );
-}
+};
+
+export default App;
